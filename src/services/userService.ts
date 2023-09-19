02f1589436634be33a28b5d14d98b23c0720ae2d
CronoCode120 <ctaverafernandez@gmail.com>
2023-09-19 22:26:43 +0200

    Co-authored-by: VictoriaGarciaFernandez <VictoriaGarciaFernandez@users.noreply.github.com> Co-authored-by: AlejandroLosilla <AlejandroLosilla@users.noreply.github.com> Co-authored-by: Fabrizio <Fyo355@users.noreply.github.com> Co-authored-by: bolthorn91 <bolthorn91@users.noreply.github.com>

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -6,6 +6,21 @@ export const getUsers = async (): Promise<any> => fetch(
 
 export const getFiveUsers = async (): Promise<any> => fetch(
     'https://jsonplaceholder.typicode.com/users?_limit=5', {
-        method: 'GET',
+        method: 'GET'
     }
 );
+
+export const newUser = (userNumber: number) => ({
+    name: `name${userNumber}`,
+    username: `username${userNumber}`
+})
+
+export const createNewUser = async (userNumber: number): Promise<any> => {
+    const user = newUser(userNumber)
+    return fetch(
+        'https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify(user)
+        }
+    )
+};
